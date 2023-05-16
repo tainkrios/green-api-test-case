54a6811a5ed3c8f9edf04d0a0d1e14d62fcb37c1
tainkrios <tain.krios@gmail.com>
2023-05-16 18:13:58 +0200

    feat: login

---
+++ src/components/login/login.jsx
@@ -0,0 +1,46 @@
+import { useRef } from 'react'
+import s from './login.module.css'
+
+export const Login = ({ setLogin }) => {
+  const idInstanceRef = useRef()
+  const apiTokenRef = useRef()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    const idInstance = idInstanceRef.current?.value
+    const apiToken = apiTokenRef.current?.value
+
+    localStorage.setItem('idInstance', idInstance)
+    localStorage.setItem('apiToken', apiToken)
+
+    console.log(idInstance)
+
+    idInstanceRef.current.value = ''
+    apiTokenRef.current.value = ''
+    setLogin(true)
+  }
+
+  return (
+    <div className={s.wrapper}>
+      <form
+        className={s.loginForm}
+        onSubmit={handleSubmit}
+      >
+        <label>IdInstance</label>
+        <input
+          ref={idInstanceRef}
+          type='text'
+          required
+        />
+        <label>ApiTokenInstance</label>
+        <input
+          ref={apiTokenRef}
+          type='text'
+          required
+        />
+        <button type='submit'>Login</button>
+      </form>
+    </div>
+  )
+}
